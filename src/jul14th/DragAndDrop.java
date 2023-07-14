258ebc99fad3e74f1d8f86785ba9726e29a3ed36
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-07-14 08:56:39 +0530

    Jul 14th Selenium Programs

---
+++ src/jul14th/DragAndDrop.java
@@ -0,0 +1,54 @@
+package jul14th;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DragAndDrop {
+	
+	WebDriver driver;
+	@BeforeMethod
+	public void setUp()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get("https://jqueryui.com/droppable/");
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void mouseHoverTest()
+	{
+		
+		driver.switchTo().frame(0);
+		
+		Actions action=new Actions(driver);
+		
+			
+		action.dragAndDrop(driver.findElement(By.xpath("//div[@id='draggable']")), 
+				                        driver.findElement(By.xpath("//div[@id='droppable']")))
+			     .build()
+		         .perform();
+		
+	}
+	
+	@AfterMethod
+	public void teardowm() throws InterruptedException
+	{
+		Thread.sleep(3000);
+		
+		driver.quit();
+	}
+	
+	
+
+}
---
+++ src/jul14th/RightClick.java
@@ -0,0 +1,51 @@
+package jul14th;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class RightClick {
+
+
+	WebDriver driver;
+	@BeforeMethod
+	public void setUp()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get("https://adactinhotelapp.com/");
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void rightClickTest()
+	{
+		
+		Actions action=new Actions(driver);
+		
+		action.contextClick(driver.findElement(By.xpath("//img[@alt='Hotel Image 3']")))
+		         .sendKeys(Keys.ENTER)
+		         .build()
+		         .perform();
+		
+	}
+	
+	@AfterMethod
+	public void teardowm() throws InterruptedException
+	{
+		Thread.sleep(3000);
+		
+		driver.quit();
+	}
+	
+	
+}
